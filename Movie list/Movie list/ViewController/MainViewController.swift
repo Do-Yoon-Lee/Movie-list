c0f74cbc74e09f21928732a6bbad64030893ae06
DoYoonLee <tlqak@naver.com>
2021-10-02 13:41:05 +0900

    MainViewController 수정

--- Movie list/Movie list/ViewController/MainViewController.swift
+++ Movie list/Movie list/ViewController/MainViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "text"
         // Do any additional setup after loading the view.
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        label.text = "Hello"
+    }
     @IBAction func pressedButton(_ sender: Any) {
         print("Hello World!!!")
     
-        button.backgroundColor = UIColor.blue
-        label.textColor = .red
     }
-   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+
 }
 
